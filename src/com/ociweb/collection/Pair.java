7ad3795f5d534fc013bc89b82373650ccf11fa6e
R. Mark Volkmann <mark@ociweb.com>
2011-02-10 10:23:10 -0600

    added missing Pair class added article templates

---
+++ src/com/ociweb/collection/Pair.java
@@ -0,0 +1,11 @@
+package com.ociweb.collection;
+
+public class Pair<K, V> {
+    public final K key;
+    public final V value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+}
